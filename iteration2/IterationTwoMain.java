27ad31319c9e1a75f421903740106781ce6cea40
Hayrullah Çağıl <43237802+hcgl@users.noreply.github.com>
2020-12-24 23:23:27 +0300

    Add files via upload
    main.java

---
+++ iteration2/IterationTwoMain.java
@@ -0,0 +1,11 @@
+package iteration2;
+
+import iteration2.handlers.SimulationHandler;
+
+public class IterationOneMain {
+
+    public static void main(String[] args) {
+        SimulationHandler simulationHandler = new SimulationHandler();
+        simulationHandler.startSimulation();
+    }
+}
