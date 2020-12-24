29d5cf9ae28b8ae9c8e5d561a38dabce44723b0b
Hayrullah Çağıl <43237802+hcgl@users.noreply.github.com>
2020-12-24 23:24:52 +0300

    Update IterationTwoMain.java

--- iteration2/IterationTwoMain.java
+++ iteration2/IterationTwoMain.java
@@ -2,7 +2,7 @@ package iteration2;
 
 import iteration2.handlers.SimulationHandler;
 
-public class IterationOneMain {
+public class IterationTwoMain {
 
     public static void main(String[] args) {
         SimulationHandler simulationHandler = new SimulationHandler();
